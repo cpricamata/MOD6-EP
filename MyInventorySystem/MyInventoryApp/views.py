b85ec6311b8c8873fe33ae786bd80fbe7837d546
haguegarcia114 <hague.garcia@student.ateneo.edu>
2026-04-16 22:09:09 +0800

    .

--- MyInventorySystem/MyInventoryApp/views.py
+++ MyInventorySystem/MyInventoryApp/views.py
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-from .models import WaterBottle 
-from .models import Supplier 
-
-# Create your views here.
-def base(request):
-    return render(request, 'MyInventoryApp/base.html')
-
-def supplier(request):
-    Supplier_objects = Supplier.objects.all()
-    return render(request, 'MyInventoryApp/supplier.html', {'suppliers':Supplier_objects})
-
-def waterbottle(request):
-    WaterBottle_objects = WaterBottle.objects.all()
-    return render(request, 'MyInventoryApp/waterbottle.html', {'wbs':WaterBottle_objects})
-
-def addwb(request):
-    WaterBottle_objects = WaterBottle.objects.all()
-    return render(request, 'MyInventoryApp/addwb.html', {'wbs':WaterBottle_objects})
-
-def view_supplier(request):
-    return render(request, 'MyInventoryApp/view_supplier.html')
-
-def view_bottles(request):
-    WaterBottle_objects = WaterBottle.objects.all()
-    return render(request, 'MyInventoryApp/view_bottles.html', {'wbs':WaterBottle_objects})
-
-def add_bottle(request):
-    return render(request, 'MyInventoryApp/add_bottle.html')
-
-
-=======
 from django.shortcuts import render
 from .models import WaterBottle 
 from .models import Supplier 
@@ -145,4 +112,3 @@ def delete_account(request, pk):
     d.delete()
     return redirect('login')
 
->>>>>>> fe0a9e5447c0f30c13efe909e7e789507cf83f70
