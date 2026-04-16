7c6d7288ebb23584d9d4be4bbe34438887f06b60
haguegarcia114 <hague.garcia@student.ateneo.edu>
2026-04-16 21:09:51 +0800

    venv in heyg's device

--- MyInventorySystem/MyInventoryApp/urls.py
+++ MyInventorySystem/MyInventoryApp/urls.py
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-"""
-URL configuration for MyInventorySystem project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', views.base, name='base'),
-    path('add_bottles',views.addwb, name="addwb"),
-    path('view_bottles',views.waterbottle, name="waterbottle"),
-    path('view_supplier',views.supplier, name="supplier"),
-]
-=======
 """
 URL configuration for MyInventorySystem project.
 
@@ -63,4 +32,3 @@ urlpatterns = [
     path('change_password/<int:pk>/', views.change_password, name='change_password'),
     path('delete_account/<int:pk>/', views.delete_account, name='delete_account'),
 ]
->>>>>>> fe0a9e5447c0f30c13efe909e7e789507cf83f70
